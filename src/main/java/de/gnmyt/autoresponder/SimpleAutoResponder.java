5e9caa7dafadf29e812aa614c2f845d5814a3b64
mathias <germannewsmaker@gmail.com>
2021-10-01 18:56:19 +0200

    Integrated an authentication method into the SimpleAutoResponder

--- src/main/java/de/gnmyt/autoresponder/SimpleAutoResponder.java
+++ src/main/java/de/gnmyt/autoresponder/SimpleAutoResponder.java
@@ -1,6 +1,7 @@
 package de.gnmyt.autoresponder;
 
 import com.sun.net.httpserver.HttpServer;
+import de.gnmyt.autoresponder.authentication.AuthenticationDetails;
 import de.gnmyt.autoresponder.exceptions.ResponderException;
 
 import java.io.IOException;
@@ -9,6 +10,7 @@ import java.net.InetSocketAddress;
 public class SimpleAutoResponder {
 
     private HttpServer httpServer;
+    private AuthenticationDetails authenticationDetails;
 
     private int port = 8025;
 
@@ -23,6 +25,20 @@ public class SimpleAutoResponder {
         } catch (IOException e) {
             throw new ResponderException("Could not open a webserver under the port " + port + ": " + e.getMessage());
         }
+    }
+
+    /**
+     * Tells the responder to use an authentication.
+     * <p>
+     * If you set this you should also set the authentication parameters in the AutoResponder app
+     *
+     * @param username The name of the user you want to use for the authentication
+     * @param password The password of the user you want to use for the authentication
+     * @return the current {@link SimpleAutoResponder} instance
+     */
+    public SimpleAutoResponder useAuthentication(String username, String password) {
+        authenticationDetails = new AuthenticationDetails(username, password);
+        return this;
     }
 
     /**
