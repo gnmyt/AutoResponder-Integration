2c5ee96c12f96b70389ed6bfae6339d6a30e3a3c
mathias <germannewsmaker@gmail.com>
2021-10-01 18:11:07 +0200

    Created the ResponderException

---
+++ src/main/java/de/gnmyt/autoresponder/exceptions/ResponderException.java
@@ -0,0 +1,14 @@
+package de.gnmyt.autoresponder.exceptions;
+
+public class ResponderException extends Exception {
+
+    /**
+     * The constructor of the {@link ResponderException}
+     *
+     * @param message The message that the {@link ResponderException} should drop
+     */
+    public ResponderException(String message) {
+        super(message);
+    }
+
+}
