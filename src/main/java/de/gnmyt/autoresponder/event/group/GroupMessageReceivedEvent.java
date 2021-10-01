f2d0d85e90ee4876b542371f230b5add10d68e76
mathias <germannewsmaker@gmail.com>
2021-10-01 23:45:21 +0200

    Created the GroupMessageReceivedEvent

---
+++ src/main/java/de/gnmyt/autoresponder/event/group/GroupMessageReceivedEvent.java
@@ -0,0 +1,57 @@
+package de.gnmyt.autoresponder.event.group;
+
+import de.gnmyt.autoresponder.SimpleAutoResponder;
+import de.gnmyt.autoresponder.event.ResponderEvent;
+import de.gnmyt.autoresponder.http.controller.HttpResponseController;
+
+public class GroupMessageReceivedEvent extends ResponderEvent {
+
+    private final String group;
+    private final String message;
+    private final String sender;
+
+    /**
+     * @param responder            The current instance of your {@link SimpleAutoResponder}
+     * @param appPackageName       The package name of the responder app
+     * @param messengerPackageName The package name of your whatsapp instance
+     * @param ruleId               The id of the rule that has been executed
+     * @param responseController   The response controller of the executed request
+     * @param group                The group in which the message has been sent
+     * @param message              The message itself
+     * @param sender               The sender which sent the message
+     */
+    public GroupMessageReceivedEvent(SimpleAutoResponder responder, String appPackageName, String messengerPackageName, int ruleId,
+                                     HttpResponseController responseController, String group, String message, String sender) {
+        super(responder, appPackageName, messengerPackageName, ruleId, responseController);
+        this.group = group;
+        this.message = message;
+        this.sender = sender;
+    }
+
+    /**
+     * Gets the group from the event
+     *
+     * @return the group from the event
+     */
+    public String getGroup() {
+        return group;
+    }
+
+    /**
+     * Gets the message from the event
+     *
+     * @return the message from the event
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * Gets the sender from the event
+     *
+     * @return the sender from the event
+     */
+    public String getSender() {
+        return sender;
+    }
+}
