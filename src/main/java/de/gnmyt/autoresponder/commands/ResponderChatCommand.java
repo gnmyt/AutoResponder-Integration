f4fa5084d936a120b9407d13f140dac497de9b15
mathias <germannewsmaker@gmail.com>
2021-10-02 20:16:19 +0200

    Updated ResponderChatCommand#execute

--- src/main/java/de/gnmyt/autoresponder/commands/ResponderChatCommand.java
+++ src/main/java/de/gnmyt/autoresponder/commands/ResponderChatCommand.java
@@ -1,9 +1,9 @@
 package de.gnmyt.autoresponder.commands;
 
-import de.gnmyt.autoresponder.entities.GroupCommandData;
+import de.gnmyt.autoresponder.entities.ChatCommand;
 
 public abstract class ResponderChatCommand extends ResponderCommand {
 
-    public abstract void execute(GroupCommandData command, Arguments args, ChatAnswerController controller);
+    public abstract void execute(ChatCommand command, Arguments args);
 
 }
