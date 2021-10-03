a93fb31bd0e36065ef80b6e53a833ecbbc6b232f
mathias <germannewsmaker@gmail.com>
2021-10-03 03:38:59 +0200

    Added the UsageExceptionType

---
+++ src/main/java/de/gnmyt/autoresponder/commands/usage/UsageExceptionType.java
@@ -0,0 +1,11 @@
+package de.gnmyt.autoresponder.commands.usage;
+
+public enum UsageExceptionType {
+
+    USAGE_NOT_COMPLETE,
+    USAGE_TYPE_MISMATCH,
+    MAX_LENGTH_EXCEEDED,
+    BELOW_MIN_LENGTH,
+    NOT_IN_ALLOWED_VALUES
+
+}
